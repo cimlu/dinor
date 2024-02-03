9a0a03571366738a9d7f06b409a4a76d7044877f
USER <sr.charada10@gmail.com>
2024-02-03 13:44:52 -0300

    Volume update

--- scripts/slider.gd
+++ scripts/slider.gd
@@ -1,5 +1,9 @@
 extends HSlider
 
+func _ready():
+	value = Sound.vol
 
 func _on_value_changed(value):
+	Sound.vol = value
+	print(value)
 	pass
--- scripts/sound.gd
+++ scripts/sound.gd
@@ -2,17 +2,22 @@ extends Node2D
 
 @onready var pickup = $pickup
 
+var vol = 0
+
 func pauseSong(songName):
 	for i in get_children().size():
 		if get_child(i).name == songName:
 			var song = get_child(i)
 			song.playing = false
-			
 
 func playSong(songName):
 	for i in get_children().size():
 		if get_child(i).name == songName:
 			var song = get_child(i)
+			if vol>50:
+				song.volume_db = vol
+			else:
+				song.volume_db = vol
 			song.play()
 
 func aumenta(songName):
